2b8521a0d350151b1f349b8d519754cda2482104
yindaheng98 <yindaheng98@163.com>
2019-05-05 22:54:34 +0800

    fixed the bug that mysql cannot accessed outside

--- supporting_files/start.sh
+++ supporting_files/start.sh
@@ -1,3 +1,5 @@
+sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+#作者疏忽，有个配置文件没改，导致外网连不上mysql
 echo '这也是我的principle'
 nohup java -jar $JETTY_HOME/start.jar > /jettybase/webapps/jetty.log  2>&1 &
 /run.sh
